f937be4cf37f96b2f7166babf1dd4367685d9a72
FuZhaohui <fuzhaohui200@gmail.com>
2014-10-21 13:57:51 +0800

    修改工具类

--- app/src/main/java/com/ces/cloudnote/app/utils/JsonUtils.java
+++ app/src/main/java/com/ces/cloudnote/app/utils/JsonUtils.java
@@ -1,52 +1,44 @@
 package com.ces.cloudnote.app.utils;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.GsonBuilder;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 /**
  * 有时候有的Gson会丢失类型信息，导致无法正常使用，目前还不知道是为什么
  * Created by remex on 14-7-16.
  */
-public class JsonUtils<T> {
+public class JsonUtils {
 
-    public JsonUtils(){
+    private static JsonUtils instance;
+    private static Gson gson;
 
+    public JsonUtils() {
+        gson = new GsonBuilder().create();
     }
 
-    public ArrayList<T> getListFromJson(String json, Class<T> clazz) {
-        if(json == null){
-            return null;
-        }
-        ArrayList<T> data = new ArrayList<T>();
-        try {
-            data = new Gson().fromJson(json, new TypeToken<ArrayList<T>>(){}.getType());
-        }catch (JsonSyntaxException e){
-            try{
-                data.add(new Gson().fromJson(json, clazz));
-            }catch (JsonSyntaxException e2) {
-                LogUtils.e(e.toString());
+    public static JsonUtils getInstance() {
+        if(instance == null) {
+            synchronized (JsonUtils.class) {
+                if(instance == null) {
+                    instance = new JsonUtils();
+                }
             }
         }
-        return data;
+        return instance;
     }
 
-    public String getJsonFromList(ArrayList<T> lists, Class<T> clazz){
-        if(lists == null)
-            return null;
-        String data = "";
-        try {
-            data = new Gson().toJson(lists, new TypeToken<ArrayList<T>>(){}.getType());
-        }catch (JsonSyntaxException e){
-            try{
-                data = new Gson().toJson(lists, clazz);
-            }catch (JsonSyntaxException e2) {
-                LogUtils.e(e.toString());
-            }
-        }
-        return data;
+    public <T> String objectToJson(Class<T> clazz) {
+        return gson.toJson(clazz);
     }
 
+    public <T> ArrayList<T> fromJsonToList(String json, Type type) {
+        return gson.fromJson(json, type);
+    }
+
+    public <T> T fromJson(String json, Class<T> clazz) {
+        return gson.fromJson(json, clazz);
+    }
 }
