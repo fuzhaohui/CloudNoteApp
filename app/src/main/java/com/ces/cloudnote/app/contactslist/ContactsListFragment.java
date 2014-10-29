bbfc3dfd523d0579c78259d83a20466f9d289f5f
FuZhaohui <fuzhaohui200@gmail.com>
2014-10-29 10:37:22 +0800

    remove anno ContactsListFragment

--- app/src/main/java/com/ces/cloudnote/app/contactslist/ContactsListFragment.java
+++ app/src/main/java/com/ces/cloudnote/app/contactslist/ContactsListFragment.java
@@ -69,13 +69,13 @@ import java.util.Locale;
 public class ContactsListFragment extends ListFragment implements
         AdapterView.OnItemClickListener, LoaderManager.LoaderCallbacks<Cursor> {
 
-    // Defines a tag for identifying log entries
     private static final String TAG = "ContactsListFragment";
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
     }
+
     private static final String STATE_PREVIOUSLY_SELECTED_KEY =
             "com.ces.cloudnote.app.contactslist.ui.SELECTED_ITEM";
 
@@ -88,7 +88,8 @@ public class ContactsListFragment extends ListFragment implements
     private boolean mIsTwoPaneLayout;
     private boolean mIsSearchResultView = false;
 
-    public ContactsListFragment() {}
+    public ContactsListFragment() {
+    }
 
     public void setSearchQuery(String query) {
         if (TextUtils.isEmpty(query)) {
@@ -126,7 +127,7 @@ public class ContactsListFragment extends ListFragment implements
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         return inflater.inflate(R.layout.contact_list_fragment, container, false);
     }
 
@@ -146,7 +147,8 @@ public class ContactsListFragment extends ListFragment implements
             }
 
             @Override
-            public void onScroll(AbsListView absListView, int i, int i1, int i2) {}
+            public void onScroll(AbsListView absListView, int i, int i1, int i2) {
+            }
         });
 
         if (mIsTwoPaneLayout) {
@@ -412,42 +414,15 @@ public class ContactsListFragment extends ListFragment implements
         private AlphabetIndexer mAlphabetIndexer; // Stores the AlphabetIndexer instance
         private TextAppearanceSpan highlightTextSpan; // Stores the highlight text appearance style
 
-        /**
-         * Instantiates a new Contacts Adapter.
-         * @param context A context that has access to the app's layout.
-         */
         public ContactsAdapter(Context context) {
             super(context, null, 0);
 
-            // Stores inflater for use later
             mInflater = LayoutInflater.from(context);
-
-            // Loads a string containing the English alphabet. To fully localize the app, provide a
-            // strings.xml file in res/values-<x> directories, where <x> is a locale. In the file,
-            // define a string with android:name="alphabet" and contents set to all of the
-            // alphabetic characters in the language in their proper sort order, in upper case if
-            // applicable.
             final String alphabet = context.getString(R.string.alphabet);
-
-            // Instantiates a new AlphabetIndexer bound to the column used to sort contact names.
-            // The cursor is left null, because it has not yet been retrieved.
             mAlphabetIndexer = new AlphabetIndexer(null, ContactsQuery.SORT_KEY, alphabet);
-
-            // Defines a span for highlighting the part of a display name that matches the search
-            // string
             highlightTextSpan = new TextAppearanceSpan(getActivity(), R.style.searchTextHiglight);
         }
 
-        /**
-         * Identifies the start of the search string in the display name column of a Cursor row.
-         * E.g. If displayName was "Adam" and search query (mSearchTerm) was "da" this would
-         * return 1.
-         *
-         * @param displayName The contact display name.
-         * @return The starting position of the search string in the display name, 0-based. The
-         * method returns -1 if the string is not found in the display name, or if the search
-         * string is empty or null.
-         */
         private int indexOfSearchQuery(String displayName) {
             if (!TextUtils.isEmpty(mSearchTerm)) {
                 return displayName.toLowerCase(Locale.getDefault()).indexOf(
@@ -456,103 +431,56 @@ public class ContactsListFragment extends ListFragment implements
             return -1;
         }
 
-        /**
-         * Overrides newView() to inflate the list item views.
-         */
         @Override
         public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
             // Inflates the list item layout.
             final View itemLayout =
                     mInflater.inflate(R.layout.contact_list_item, viewGroup, false);
 
-            // Creates a new ViewHolder in which to store handles to each view resource. This
-            // allows bindView() to retrieve stored references instead of calling findViewById for
-            // each instance of the layout.
             final ViewHolder holder = new ViewHolder();
             holder.text1 = (TextView) itemLayout.findViewById(android.R.id.text1);
             holder.text2 = (TextView) itemLayout.findViewById(android.R.id.text2);
             holder.icon = (QuickContactBadge) itemLayout.findViewById(android.R.id.icon);
 
-            // Stores the resourceHolder instance in itemLayout. This makes resourceHolder
-            // available to bindView and other methods that receive a handle to the item view.
             itemLayout.setTag(holder);
 
-            // Returns the item layout view
             return itemLayout;
         }
 
-        /**
-         * Binds data from the Cursor to the provided view.
-         */
         @Override
         public void bindView(View view, Context context, Cursor cursor) {
             // Gets handles to individual view resources
             final ViewHolder holder = (ViewHolder) view.getTag();
 
-            // For Android 3.0 and later, gets the thumbnail image Uri from the current Cursor row.
-            // For platforms earlier than 3.0, this isn't necessary, because the thumbnail is
-            // generated from the other fields in the row.
             final String photoUri = cursor.getString(ContactsQuery.PHOTO_THUMBNAIL_DATA);
-
             final String displayName = cursor.getString(ContactsQuery.DISPLAY_NAME);
-
             final int startIndex = indexOfSearchQuery(displayName);
 
             if (startIndex == -1) {
-                // If the user didn't do a search, or the search string didn't match a display
-                // name, show the display name without highlighting
                 holder.text1.setText(displayName);
-
                 if (TextUtils.isEmpty(mSearchTerm)) {
-                    // If the search search is empty, hide the second line of text
                     holder.text2.setVisibility(View.GONE);
                 } else {
-                    // Shows a second line of text that indicates the search string matched
-                    // something other than the display name
                     holder.text2.setVisibility(View.VISIBLE);
                 }
             } else {
-                // If the search string matched the display name, applies a SpannableString to
-                // highlight the search string with the displayed display name
-
-                // Wraps the display name in the SpannableString
                 final SpannableString highlightedName = new SpannableString(displayName);
-
-                // Sets the span to start at the starting point of the match and end at "length"
-                // characters beyond the starting point
                 highlightedName.setSpan(highlightTextSpan, startIndex,
                         startIndex + mSearchTerm.length(), 0);
 
-                // Binds the SpannableString to the display name View object
                 holder.text1.setText(highlightedName);
 
-                // Since the search string matched the name, this hides the secondary message
                 holder.text2.setVisibility(View.GONE);
             }
-
-            // Processes the QuickContactBadge. A QuickContactBadge first appears as a contact's
-            // thumbnail image with styling that indicates it can be touched for additional
-            // information. When the user clicks the image, the badge expands into a dialog box
-            // containing the contact's details and icons for the built-in apps that can handle
-            // each detail type.
-
-            // Generates the contact lookup Uri
             final Uri contactUri = Contacts.getLookupUri(
                     cursor.getLong(ContactsQuery.ID),
                     cursor.getString(ContactsQuery.LOOKUP_KEY));
 
-            // Binds the contact's lookup Uri to the QuickContactBadge
             holder.icon.assignContactUri(contactUri);
 
-            // Loads the thumbnail image pointed to by photoUri into the QuickContactBadge in a
-            // background worker thread
             mImageLoader.loadImage(photoUri, holder.icon);
         }
 
-        /**
-         * Overrides swapCursor to move the new Cursor into the AlphabetIndex as well as the
-         * CursorAdapter.
-         */
         @Override
         public Cursor swapCursor(Cursor newCursor) {
             // Update the AlphabetIndexer with new cursor as well
@@ -560,10 +488,6 @@ public class ContactsListFragment extends ListFragment implements
             return super.swapCursor(newCursor);
         }
 
-        /**
-         * An override of getCount that simplifies accessing the Cursor. If the Cursor is null,
-         * getCount returns zero. As a result, no test for Cursor == null is needed.
-         */
         @Override
         public int getCount() {
             if (getCursor() == null) {
@@ -572,17 +496,11 @@ public class ContactsListFragment extends ListFragment implements
             return super.getCount();
         }
 
-        /**
-         * Defines the SectionIndexer.getSections() interface.
-         */
         @Override
         public Object[] getSections() {
             return mAlphabetIndexer.getSections();
         }
 
-        /**
-         * Defines the SectionIndexer.getPositionForSection() interface.
-         */
         @Override
         public int getPositionForSection(int i) {
             if (getCursor() == null) {
@@ -591,9 +509,6 @@ public class ContactsListFragment extends ListFragment implements
             return mAlphabetIndexer.getPositionForSection(i);
         }
 
-        /**
-         * Defines the SectionIndexer.getSectionForPosition() interface.
-         */
         @Override
         public int getSectionForPosition(int i) {
             if (getCursor() == null) {
@@ -602,11 +517,6 @@ public class ContactsListFragment extends ListFragment implements
             return mAlphabetIndexer.getSectionForPosition(i);
         }
 
-        /**
-         * A class that defines fields for each resource ID in the list item layout. This allows
-         * ContactsAdapter.newView() to store the IDs once, when it inflates the layout, instead of
-         * calling findViewById in each iteration of bindView.
-         */
         private class ViewHolder {
             TextView text1;
             TextView text2;
@@ -614,83 +524,35 @@ public class ContactsListFragment extends ListFragment implements
         }
     }
 
-    /**
-     * This interface must be implemented by any activity that loads this fragment. When an
-     * interaction occurs, such as touching an item from the ListView, these callbacks will
-     * be invoked to communicate the event back to the activity.
-     */
     public interface OnContactsInteractionListener {
-        /**
-         * Called when a contact is selected from the ListView.
-         * @param contactUri The contact Uri.
-         */
+
         public void onContactSelected(Uri contactUri);
 
-        /**
-         * Called when the ListView selection is cleared like when
-         * a contact search is taking place or is finishing.
-         */
         public void onSelectionCleared();
     }
 
-    /**
-     * This interface defines constants for the Cursor and CursorLoader, based on constants defined
-     * in the {@link android.provider.ContactsContract.Contacts} class.
-     */
     public interface ContactsQuery {
 
-        // An identifier for the loader
         final static int QUERY_ID = 1;
-
-        // A content URI for the Contacts table
         final static Uri CONTENT_URI = Contacts.CONTENT_URI;
-
-        // The search/filter query Uri
         final static Uri FILTER_URI = Contacts.CONTENT_FILTER_URI;
 
-        // The selection clause for the CursorLoader query. The search criteria defined here
-        // restrict results to contacts that have a display name and are linked to visible groups.
-        // Notice that the search on the string provided by the user is implemented by appending
-        // the search string to CONTENT_FILTER_URI.
         @SuppressLint("InlinedApi")
         final static String SELECTION =
                 (Utils.hasHoneycomb() ? Contacts.DISPLAY_NAME_PRIMARY : Contacts.DISPLAY_NAME) +
-                "<>''" + " AND " + Contacts.IN_VISIBLE_GROUP + "=1";
+                        "<>''" + " AND " + Contacts.IN_VISIBLE_GROUP + "=1";
 
-        // The desired sort order for the returned Cursor. In Android 3.0 and later, the primary
-        // sort key allows for localization. In earlier versions. use the display name as the sort
-        // key.
         @SuppressLint("InlinedApi")
         final static String SORT_ORDER =
                 Utils.hasHoneycomb() ? Contacts.SORT_KEY_PRIMARY : Contacts.DISPLAY_NAME;
 
-        // The projection for the CursorLoader query. This is a list of columns that the Contacts
-        // Provider should return in the Cursor.
         @SuppressLint("InlinedApi")
         final static String[] PROJECTION = {
 
-                // The contact's row id
                 Contacts._ID,
-
-                // A pointer to the contact that is guaranteed to be more permanent than _ID. Given
-                // a contact's current _ID value and LOOKUP_KEY, the Contacts Provider can generate
-                // a "permanent" contact URI.
                 Contacts.LOOKUP_KEY,
-
-                // In platform version 3.0 and later, the Contacts table contains
-                // DISPLAY_NAME_PRIMARY, which either contains the contact's displayable name or
-                // some other useful identifier such as an email address. This column isn't
-                // available in earlier versions of Android, so you must use Contacts.DISPLAY_NAME
-                // instead.
                 Utils.hasHoneycomb() ? Contacts.DISPLAY_NAME_PRIMARY : Contacts.DISPLAY_NAME,
-
-                // In Android 3.0 and later, the thumbnail image is pointed to by
-                // PHOTO_THUMBNAIL_URI. In earlier versions, there is no direct pointer; instead,
-                // you generate the pointer from the contact's ID value and constants defined in
-                // android.provider.ContactsContract.Contacts.
                 Utils.hasHoneycomb() ? Contacts.PHOTO_THUMBNAIL_URI : Contacts._ID,
-
-                // The sort order column for the returned Cursor, used by the AlphabetIndexer
                 SORT_ORDER,
         };
 
